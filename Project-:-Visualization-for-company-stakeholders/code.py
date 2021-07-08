60db460b8bbd07a73d7b2bbcbd0e1d63b945b3bc
Swapnil patil <81431059+swapnilbpatil@users.noreply.github.com>
2021-07-08 23:39:22 +0530

    Code file created.

---
+++ Project-:-Visualization-for-company-stakeholders/code.py
@@ -0,0 +1,80 @@
+# --------------
+#Code starts here
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv(path)
+loan_status = data['Loan_Status'].value_counts()
+loan_status.plot(kind='bar')
+#Code starts here
+
+
+# --------------
+#Code starts here
+
+
+
+
+property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
+print(property_and_loan)
+property_and_loan.plot(kind='bar',stacked=False)
+plt.xlabel('Property Area')
+plt.ylabel('Loan status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+
+# --------------
+#Code starts here
+education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
+print(education_and_loan)
+education_and_loan.plot(kind='bar',stacked=True)
+plt.xlabel('Education status')
+plt.ylabel('Loan status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education']=='Graduate']
+not_graduate=data[data['Education']=='Not Graduate']
+graduate['LoanAmount'].plot(kind='density',label='Graduate')
+not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
+
+
+
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+
+
+
+
+fig ,(ax_1,ax_2,ax_3) = plt.subplots(nrows=3, ncols=1,figsize=(20,20))
+ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
+ax_1.set(title='Applicant Income')
+ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
+ax_2.set(title='Coapplicant Income')
+data['TotalIncome'] = data['ApplicantIncome'] + data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
+ax_3.set(title='Total Income')
+
+
